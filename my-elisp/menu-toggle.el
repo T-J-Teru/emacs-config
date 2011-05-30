3cdf6a476892be1f966873c4202aa8337dc082c8
Andrew Burgess <aburgess@broadcom.com>
2011-05-30 09:43:30 +0100

    Add function for toggling menu and tool-bar in sequence.

---
+++ my-elisp/menu-toggle.el
@@ -0,0 +1,24 @@
+;; Function to toggle menu and tool bars on and off.
+(defun toggle-menubar-and-toolbar ()
+  "Switches off both menu and tool bar."
+  (interactive)
+  (if window-system
+      ;; In X mode we have menu-bar and tool-bar, rotate through all
+      ;; the combinations of having each on or off. Using "M" for
+      ;; menu-bar on, and "m" for menu-bar off, and "T" for tool-bar
+      ;; on, and "t" for tool-bar off, then the sequence is:
+      ;;      (m,t)->(M,t)->(M,T)->(m,T)->(m,t)
+      (if tool-bar-mode 
+          (if menu-bar-mode
+              (menu-bar-mode -1)
+            (tool-bar-mode -1))
+        (if menu-bar-mode 
+            (tool-bar-mode 1)
+          (menu-bar-mode 1)))
+    ;; In terminal mode we only have the menu-bar, just toggle it on
+    ;; and off.
+    (if menu-bar-mode
+        (menu-bar-mode -1)
+      (menu-bar-mode 1))))
+
+(provide 'menu-toggle)
